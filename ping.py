9bfeba666ed48bde7381a860032eac379b3e5269
Yorai Shaoul <yorai@mit.edu>
2018-11-22 00:30:02 -0500

    added ping functionality. Ping status and time is stored in a text file on the server

--- ping.py
+++ ping.py
@@ -14,6 +14,26 @@ inDataDict = cgi.FieldStorage()
 #ask that the info will bi in the form of asdfgefs/dataHandler.py?status='alive' OR 'filming'
 if 'status' in inDataDict:
     # modify the txt file
-    print('Status is', inDataDict['status']['status'], 'at', datetime.datetime.now())
+    with open('status.txt', 'w') as f:
+        # re-write the file with the current data we got:
+        month_dict = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep',10:'Oct', 11:'Nov', 12:'Dec'}
+        now = datetime.datetime.now()
+        year = now.year
+        day = now.day 
+        date = month_dict[now.month] +' '+ str(day)+' ' + str(year)
+        
+        hour = ('0'+ str(now.hour))[-2:] + ':' +('0'+ str(now.minute))[-2:] +':'+ ('0'+ str(now.second))[-2:]
 
-# print the txt file
+
+
+        new_data_string = 'Status is ~~' +  inDataDict.getvalue('status') + '~~ at '+hour+' '+ date + ' Boston Time'
+        f.write(new_data_string)
+
+# now, whether or not we modified the file, we will print the file out, since this is the most recent update
+
+with open ('status.txt', 'r') as f:
+    data = f.read()
+    print(data)
+
+
+
