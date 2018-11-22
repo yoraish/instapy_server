5654d6569d210f1329c2fa58accd3459d341f6e2
Yorai Shaoul <yoraish@Yorais-MacBook-Pro.local>
2018-11-21 23:29:44 -0500

    starting to implement

---
+++ ping.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+print( "Content-type: text/html\n")
+import cgitb
+import cgi
+import datetime
+import sqlite3
+import datetime
+cgitb.enable()
+
+#command below takes the arguements from the get request and puts them in some sort of a dictionary
+
+inDataDict = cgi.FieldStorage()
+
+#ask that the info will bi in the form of asdfgefs/dataHandler.py?status='alive' OR 'filming'
+if 'status' in inDataDict:
+    # modify the txt file
+    print('Status is', inDataDict['status'], 'at', datetime.datetime.now())
+
+# print the txt file
